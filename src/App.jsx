effc1d913a583933ee1ef40c8d11bbfa622aeeb7
rajurao <rajurao1107@gmail.com>
2024-08-07 20:11:32 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -17,14 +18,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <nav>
+        <Link to={"/sp-advertising-demo4/"}>home</Link>
+        {" | "}
+        <Link to={"/sp-advertising-demo4/contact"}>contact</Link>
+      </nav>
+      
+      <Outlet/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:"/sp-advertising-demo4/",
+    element:<App/>,
+    children: [
+      {
+        path:"/sp-advertising-demo4/",
+        element:<Home/>
+      },
+      {
+        path:"/sp-advertising-demo4/contact",
+        element:<Contact/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
